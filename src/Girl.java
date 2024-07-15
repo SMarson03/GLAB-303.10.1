f4931c13558c28b445e631b9265b3025ded66c2d
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-15 18:17:20 -0400

    1

---
+++ src/Girl.java
@@ -0,0 +1,17 @@
+public class Girl extends Person {
+    static double ageFactor = 1.3;
+    public Girl(String aName) {
+        name = "Ms." + aName;
+    }
+
+    @Override
+    public String talk() {
+        return ("Hello!" + jump());
+    }
+    public String jump(){
+        return ("I am jumping.");
+    }
+    public static double lifespan(){
+        return (LifeSpan * ageFactor);
+    }
+}
